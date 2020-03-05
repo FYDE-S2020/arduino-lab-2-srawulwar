0e99e0a492d68867aa9e9359dd21b1ef6c7e5dca
Saiteja Rawulwar <srawulwar@utexas.edu>
2020-03-05 17:53:49 -0600

    Starting Code

--- BlynkStarter/BlynkStarter.ino
+++ BlynkStarter/BlynkStarter.ino
@@ -31,7 +31,7 @@
 
 /* Comment this out to disable prints and save space */
 #define BLYNK_PRINT Serial
-
+#include <Blynk.h>
 
 #include <WiFi.h>
 #include <WiFiClient.h>
@@ -45,17 +45,87 @@ char auth[] = "YBlThDhEwdbxvVQIZ26tAwF7YjImhqPe";
 // Set password to "" for open networks.
 // The EE IOT network is hidden. You might not be able to see it.
 // But you should be able to connect with these credentials. 
-char ssid[32] = "Sai's iPhone";
-char pass[32] = "Quanticore007";
+char ssid[32] = "EE-IOT-Platform-02";
+char pass[32] = "g!TyA>hR2JTy";
+#define LED 2
 
+const int freq = 5000;     // 5KHz frequency is more than enough. Remember we used 100 before.
+const int ledChannel = 0;  // You can have up to 16 pwm channels (0 to 15)
+const int resolution = 10; // 10 bit resolution for 1023. Freq should be greater than resolution.
+BlynkTimer timer;
+int time_count = 0; // timer counter global variable
+String content = "";  // null string constant ( an empty string )
+
+void myTimerEvent() // Every 10 ms
+{
+    if (time_count % 25 == 0) { // every 250 ms
+        // Do thing that needs to happen every 0.25 seconds
+    }
+    if (time_count == 100){
+        // Do thing that needs to happen every 1 second
+        time_count = 0; // reset time counter
+    }
+    else {
+        // Send serial data to Blynk terminal
+        char character;
+        while(Serial.available()) { // Check if serial is available every 10 ms
+            character = Serial.read();
+            content.concat(character);
+        }
+        if (content != "") {
+            Blynk.virtualWrite(virtual_pin, content);
+            content = ""; // Clear String
+        }  
+    }
+    time_count += 1; // Increment on every tick
+}
 void setup()
 {
   // Serial Monitor
   Serial.begin(115200);
   Blynk.begin(auth, ssid, pass);
+    // configure LED PWM functionality
+    ledcSetup(ledChannel, freq, resolution);
+
+    // attach the channel to the GPIO to be controlled
+    ledcAttachPin(LED, ledChannel);
+        // Setup a function to be called every second
+    timer.setInterval(1000L, myTimerEvent);
 }
 
+
+// This function sends Arduino's up time every second to Virtual Pin 0.
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+void myTimerEvent()
+{
+// You can send any value at any time.
+// Don't send more than 10 values a second or the Blynk server will block you!
+    Blynk.virtualWrite(V0, millis() / 1000); // Write the arduino uptime every second
+}
+BLYNK_WRITE(V1)
+{
+
+    // param is a member variable of the Blynk ADT. It is exposed so you can read it.
+    int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
+
+    // Because V1 is a button, pinValue will be a 0 or a 1.
+    if (pinValue == 0) {
+        // turn LED off
+        digitalWrite(LED_PIN, LOW);
+    }
+    else {
+        digitalWrite(LED_PIN, HIGH);
+    }
+}
+// This function is executed whenever Blynk pin V5 requests data from the ESP32
+BLYNK_READ(V5) // Widget in the app READS Virtal Pin V5 with the certain frequency
+{
+    // This command writes Arduino's uptime in seconds to Virtual Pin V5
+    Blynk.virtualWrite(5, millis() / 1000);
+}
 void loop()
 {
   Blynk.run();
+  timer.run();
 }
